436d32aee78550c74abad90f7d6e1e7ef2e379ef
NEKKAA JESSIM p1805614 <jessim.nekkaa@etu.univ-lyon1.fr>
2020-10-10 14:57:54 +0200

    Ajout de l'authentification Client

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './Authentification/auth/auth.component';
+import { LoginComponent } from './Authentification/login/login.component';
+import { SigninComponent } from './Authentification/signin/signin.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    LoginComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
