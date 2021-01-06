2ed698fff9051e04ace2fcc6a3409d4ef3d37e9a
SAURY JEAN p1805563 <jean.saury@etu.univ-lyon1.fr>
2021-01-06 16:25:57 +0100

    Labyrinthe reçu client console

--- Frontend/src/app/Services/Game/labirynthe/labirynthe.service.ts
+++ Frontend/src/app/Services/Game/labirynthe/labirynthe.service.ts
@@ -11,25 +11,46 @@ import { LabiryntheGameService } from './labirynthe-game.service';
 
 export class LabiryntheService extends Phaser.Scene{
 
-  labiryntheService;
+  map;
+
+  labiryntheGameService;
 
   constructor(labiryntheGameService : LabiryntheGameService) {
     super({ key: 'labirynthe' });
-    this.labiryntheService = labiryntheGameService
+    this.labiryntheGameService = labiryntheGameService;
 
   }
 
   preload() {
-    
+    this.map = this.labiryntheGameService.roomService.laby;
+
+    function printMap(map) {
+    	let aff = String("");
+    	for(let i = 0; i < map.length; i++) {
+        for(let j = 0; j < map[0].length; j++) {
+          if(map[i][j] === 0) {
+            aff += "██";
+          }
+          else {
+            aff += "  ";
+          }
+        }
+        aff += "\n";
+      }
+      return aff
+    }
+
+    console.log(printMap(this.map));
+    //ici on charge les images
   }
 
   create() {
-
+    //ici on les affiches
   }
 
 
   update() {
-
+    //ici les déplacements
   }
 
 }
